800ff491e2cb764224956e8f59b2a36efbe0727c
Claudio Schaepper <claudio.schaepper@gmail.com>
2015-06-05 09:04:58 +0200

    Add

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		url:'https://api.forecast.io/forecast/2c3248b853cc8517e69d06caa732df2a/47.4145192,9.3141141?callback=?&units=si'
 	}).done(function(data){
 		console.log(data);
-		$('h1').text(data.currently.temperature + 'C');
-		$('p').text(data.flags['metno-license']);
+		$('#temp').text(data.currently.temperature + 'C');
+		$('#license').text(data.flags['metno-license']);
 	});
 })
