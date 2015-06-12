af023ba08045473d0158b47146561c1bfd98533c
Claudio Schaepper <claudio.schaepper@gmail.com>
2015-06-12 09:18:15 +0200

    1

--- script.js
+++ script.js
@@ -2,31 +2,26 @@ $(document).ready(function(){
 
 	navigator.geolocation.getCurrentPosition(function(position){
 		var koordinaten = {longitude: position.coords.longitude, latitude: position.coords.latitude};
+			
+		$.ajax({
+			url: 'https://api.forecast.io/forecast/2c3248b853cc8517e69d06caa732df2a/'+koordinaten.latitude+','+koordinaten.longitude,
+			data:{
+			units: 'si',
+			lang: 'de'
+			},
+			dataType: 'jsonp'
+			}).done(function(data){
+			console.log(data);
+	});
 
-		console.log(koordinaten.longitude);
-		//console.log(position);
-		
 
+	});
+
+
+});
 
 		/*
 		$('.longitude').text(position.coords.longitude);
 		$('.latitude').text(position.coords.latitude);
 		$('.accuracy').text(position.coords.accuracy);
 		*/
-	});
-
-	$.ajax({
-		url: 'https://api.forecast.io/forecast/2c3248b853cc8517e69d06caa732df2a/'+koordinaten.latitude+','+koordinaten.longitude,
-		data:{
-			units: 'si',
-			lang: 'de'
-		},
-		dataType: 'jsonp'
-	}).done(function(data){
-		console.log(data);
-	})
-
-
-
-
-});
