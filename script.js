1519369fa2620c3f94978bcd6da7696b168eb8c9
Claudio Schaepper <claudio.schaepper@gmail.com>
2015-06-19 10:22:41 +0200

    ff

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 			$('.temperature').text(data.currently.apparentTemperature+' °C');
 			$('.wettertext').text(data.currently.summary);
 			$('.tempMAX').text('Temperatur max: '+data.daily.data[0].apparentTemperatureMax+' °C | Temperatur min: '+ data.daily.data[0].apparentTemperatureMin);
-			$('.license').text(data.flags.metno-license);
+			$('.license').text(data.flags[metno-license]);
 			icon= data.currently.icon.toUpperCase();
 			console.log(icon);
 			console.log(data);
